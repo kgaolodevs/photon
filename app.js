dceda09124a3039bed6aa13b17181c1da6804384
kgaolodevs <kgaolodev@gmail.com>
2020-07-10 12:40:28 +0200

    Started search functionality

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const app = {
   auth: "563492ad6f917000010000018b7a2a1d03e34eafb17fc6c055be806e",
   gallery: document.querySelector(".app__gallery"),
   input: document.querySelector(".app__searchForm--search"),
-  submitButton: document.querySelector(".app__searchForm--submitButton"),
-  seachValue: null,
+  searchForm: document.querySelector(".app__searchForm"),
+  searchValue: null,
 };
 
 const actions = {
@@ -29,9 +29,40 @@ const actions = {
       app.gallery.appendChild(galleryImage);
     });
   },
+
+  searchPhotos: async function (query) {
+    const dataFetch = await fetch(
+      `https://api.pexels.com/v1/search?query=${query}&per_page=15`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: app.auth,
+        },
+      }
+    );
+    const data = await dataFetch.json();
+    data.photos.forEach((photo) => {
+      const galleryImage = document.createElement("div");
+      galleryImage.classList.add("gallery-image");
+      galleryImage.innerHTML = `
+      <img src=${photo.src.large}> 
+      <p>${photo.photographer}</p>
+      `;
+      app.gallery.appendChild(galleryImage);
+    });
+  },
+
+  updateInput: function (e) {
+    app.searchValue = e.target.value;
+  },
 };
 
 actions.curatedPhotos();
 
 // Event listeners
-app.submitButton.addEventListener("click", actions.curatedPhotos);
+app.input.addEventListener("input", actions.updateInput);
+app.searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  actions.searchPhotos(app.input.value);
+});
